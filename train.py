61694d350cbb96665f7358e50586f14d4d0fc135
Antonín Jarolím <jarolim.antonin@seznam.cz>
2025-03-07 01:11:41 +0100

    Prepare train on HPC

--- train.py
+++ train.py
@@ -7,16 +7,18 @@ from colbert.infra.config import ColBERTConfig, RunConfig
 from colbert import Trainer
 
 
-def train(experiment):
-    with Run().context(RunConfig(nranks=2, experiment=experiment)):
+def train(experiment, ngpus):
+    with Run().context(RunConfig(nranks=ngpus, experiment=experiment)):
         triples_path = 'data/training/examples_with_relevancy.jsonl'
         queries_path = 'data/training/queries.train.tsv'
         collection_path = 'data/training/collection.tsv'
         extractions_path = 'data/training/extracted_relevancy_800k_unique.tsv'
 
-        config = ColBERTConfig(bsize=128, lr=1e-05, warmup=20_000, doc_maxlen=180, dim=128, attend_to_mask_tokens=False,
+        one_bsize = 128
+        config = ColBERTConfig(bsize=one_bsize * ngpus, lr=1e-05, warmup=20_000,
+                               doc_maxlen=180, dim=128, attend_to_mask_tokens=False,
                                return_max_scores=True,
-                               nway=64, accumsteps=32, similarity='cosine', use_ib_negatives=True)
+                               nway=64, accumsteps=2, similarity='cosine', use_ib_negatives=True)
         trainer = Trainer(triples=triples_path, queries=queries_path, collection=collection_path,
                           extractions=extractions_path, config=config)
 
@@ -27,9 +29,10 @@ def arg_parse():
     import argparse
     parser = argparse.ArgumentParser(description='Train ColBERT with extractions')
     parser.add_argument('--experiment', type=str, required=True, help='Experiment name')
+    parser.add_argument('--ngpus', type=int, default=2, help='Number of GPUs')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = arg_parse()
-    train(args.experiment)
+    train(args.experiment, args.ngpus)
