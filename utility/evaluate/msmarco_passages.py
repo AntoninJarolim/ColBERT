fa5c2387ec0e5d5cc8adcf9e5af9ea776c3e5403
Antonín Jarolím <jarolim.antonin@seznam.cz>
2025-03-08 19:59:52 +0100

    Return ranking evaluation

--- utility/evaluate/msmarco_passages.py
+++ utility/evaluate/msmarco_passages.py
@@ -10,36 +10,47 @@ import random
 
 from argparse import ArgumentParser
 from collections import defaultdict
+
+from colbert.data import TranslateAbleCollection
 from colbert.utils.utils import print_message, file_tqdm
 
 
-def main(args):
+def main(args_collection, args_output, args_qrels, args_ranking, args_annotate):
     qid2positives = defaultdict(list)
     qid2ranking = defaultdict(list)
     qid2mrr = {}
     qid2recall = {depth: {} for depth in [50, 200, 1000, 5000, 10000]}
 
-    with open(args.qrels) as f:
-        print_message(f"#> Loading QRELs from {args.qrels} ..")
+    collection = None
+    if args_collection:
+        collection = TranslateAbleCollection(path=args_collection)
+
+    with open(args_qrels) as f:
+        print_message(f"#> Loading QRELs from {args_qrels} ..")
         for line in file_tqdm(f):
             qid, _, pid, label = map(int, line.strip().split())
             assert label == 1
 
             qid2positives[qid].append(pid)
 
-    with open(args.ranking) as f:
-        print_message(f"#> Loading ranked lists from {args.ranking} ..")
+    with open(args_ranking) as f:
+        print_message(f"#> Loading ranked lists from {args_ranking} ..")
+
+        results = defaultdict(list)
         for line in file_tqdm(f):
-            qid, pid, rank, *score = line.strip().split('\t')
-            qid, pid, rank = int(qid), int(pid), int(rank)
+            qid, *rest = line.strip().split('\t')
+            results[int(qid)].append(rest)
 
-            if len(score) > 0:
-                assert len(score) == 1
-                score = float(score[0])
-            else:
-                score = None
+        for qid, result_data in results.items():
+            assert len(result_data) == 3
+            pids = result_data[0]
+            ranks = result_data[1]
+            scores = result_data[2]
 
-            qid2ranking[qid].append((rank, pid, score))
+            for pid, rank, score in zip(pids, ranks, scores):
+                if collection:
+                    pid = collection.translate(int(pid))
+                qid2ranking[qid].append((int(rank), int(pid), float(score)))
 
     assert set.issubset(set(qid2ranking.keys()), set(qid2positives.keys()))
 
@@ -77,11 +88,13 @@ def main(args):
     assert len(qid2mrr) <= num_ranked_queries, (len(qid2mrr), num_ranked_queries)
 
     print()
-    mrr_10_sum = sum(qid2mrr.values())
-    print_message(f"#> MRR@10 = {mrr_10_sum / num_judged_queries}")
-    print_message(f"#> MRR@10 (only for ranked queries) = {mrr_10_sum / num_ranked_queries}")
+    mrr_10 = sum(qid2mrr.values()) / num_judged_queries
+    mrr_10_ranked = sum(qid2mrr.values()) / num_ranked_queries
+    print_message(f"#> MRR@10 = {mrr_10}")
+    print_message(f"#> MRR@10 (only for ranked queries) = {mrr_10_ranked}")
     print()
 
+    dict_out = {"mrr@10": mrr_10}
     for depth in qid2recall:
         assert len(qid2recall[depth]) <= num_ranked_queries, (len(qid2recall[depth]), num_ranked_queries)
 
@@ -91,10 +104,12 @@ def main(args):
         print_message(f"#> Recall@{depth} (only for ranked queries) = {metric_sum / num_ranked_queries}")
         print()
 
-    if args.annotate:
-        print_message(f"#> Writing annotations to {args.output} ..")
+        dict_out[f"recall@{depth}"] = metric_sum / num_judged_queries
 
-        with open(args.output, 'w') as f:
+    if args_annotate:
+        print_message(f"#> Writing annotations to {args_output} ..")
+
+        with open(args_output, 'w') as f:
             for qid in tqdm.tqdm(qid2positives):
                 ranking = qid2ranking[qid]
                 positives = qid2positives[qid]
@@ -108,6 +123,8 @@ def main(args):
                     line = '\t'.join(map(str, line)) + '\n'
                     f.write(line)
 
+    return dict_out
+
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="msmarco_passages.")
@@ -116,11 +133,13 @@ if __name__ == "__main__":
     parser.add_argument('--qrels', dest='qrels', required=True, type=str)
     parser.add_argument('--ranking', dest='ranking', required=True, type=str)
     parser.add_argument('--annotate', dest='annotate', default=False, action='store_true')
+    parser.add_argument('--collection', dest='collection', type=str, default=None)
 
     args = parser.parse_args()
 
+    args.output = None
     if args.annotate:
         args.output = f'{args.ranking}.annotated'
         assert not os.path.exists(args.output), args.output
 
-    main(args)
+    main(args.collection, args.output, args.qrels, args.ranking, args.annotate)
