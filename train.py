ce5edce4be63f47e5e476b9ed2da1f42aed7ce48
Antonín Jarolím <jarolim.antonin@seznam.cz>
2025-03-18 22:06:03 +0100

    Add accumsteps param

--- train.py
+++ train.py
@@ -7,18 +7,18 @@ from colbert.infra.config import ColBERTConfig, RunConfig
 from colbert import Trainer
 
 
-def train(experiment, ngpus, ex_lambda):
+def train(experiment, ngpus, ex_lambda, accumsteps):
     with Run().context(RunConfig(nranks=ngpus, experiment=experiment)):
         triples_path = 'data/training/examples_with_relevancy.jsonl'
         queries_path = 'data/training/queries.train.tsv'
         collection_path = 'data/training/collection.tsv'
         extractions_path = 'data/training/extracted_relevancy_800k_unique.tsv'
 
-        one_bsize = 128
+        one_bsize = 64
         config = ColBERTConfig(bsize=one_bsize * ngpus, lr=1e-05, warmup=20_000,
                                doc_maxlen=180, dim=128, attend_to_mask_tokens=False,
                                return_max_scores=True, extractions_lambda=ex_lambda,
-                               nway=64, accumsteps=2, similarity='cosine', use_ib_negatives=True)
+                               nway=64, accumsteps=accumsteps, similarity='cosine', use_ib_negatives=True)
         trainer = Trainer(triples=triples_path, queries=queries_path, collection=collection_path,
                           extractions=extractions_path, config=config)
 
@@ -31,10 +31,11 @@ def arg_parse():
     parser.add_argument('--experiment', type=str, required=True, help='Experiment name')
     parser.add_argument('--ngpus', type=int, default=2, help='Number of GPUs')
     parser.add_argument('--ex_lambda', type=float, default=0.5, help='Extractions lambda')
+    parser.add_argument('--accumsteps', type=int, default=2, help='Number of gradient accumulation steps')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = arg_parse()
-    train(args.experiment, args.ngpus, args.ex_lambda)
+    train(args.experiment, args.ngpus, args.ex_lambda, args.accumsteps)
 
